cdac2d62f70dd9eea5affbb98090566e7d040cb3
Eugene Cheipesh <echeipesh@gmail.com>
2016-07-11 17:40:07 -0400

    using deploy.sh

---
+++ .travis/deploy.sh
@@ -0,0 +1,3 @@
+docker push quay.io/${QUAY_ORG}/${QUAY_REPO}:${QUAY_TAG}
+docker tag -f quay.io/${QUAY_ORG}/${QUAY_REPO}:${QUAY_TAG} quay.io/${QUAY_ORG}/${QUAY_REPO}:latest
+docker push quay.io/${QUAY_ORG}/${QUAY_REPO}:latest
