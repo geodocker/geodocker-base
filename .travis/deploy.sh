f61c1605ddebd3a5ea54f5ab363da7c1db51bce3
Eugene Cheipesh <echeipesh@gmail.com>
2016-07-12 12:09:52 -0400

    grab variables from environment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
-set -x
+set -e
 
 docker push quay.io/${QUAY_ORG}/${QUAY_REPO}:${QUAY_TAG}
-docker tag -f quay.io/${QUAY_ORG}/${QUAY_REPO}:${QUAY_TAG} quay.io/${QUAY_ORG}/${QUAY_REPO}:latest
+docker tag quay.io/${QUAY_ORG}/${QUAY_REPO}:${QUAY_TAG} quay.io/${QUAY_ORG}/${QUAY_REPO}:latest
 docker push quay.io/${QUAY_ORG}/${QUAY_REPO}:latest
