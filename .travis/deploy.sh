cf0e20c962b2f1c5d468ab3fe28c498084b5c242
Eugene Cheipesh <echeipesh@gmail.com>
2016-07-12 12:21:58 -0400

    fix: adding hash bang

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
+
 set -e
+
+QUAY_ORG=geotrellis
+QUAY_REPO=${TRAVIS_REPO_SLUG##*/}
+QUAY_TAG=${TRAVIS_TAG:-${TRAVIS_COMMIT:0:7}}
 
 docker push quay.io/${QUAY_ORG}/${QUAY_REPO}:${QUAY_TAG}
 docker tag quay.io/${QUAY_ORG}/${QUAY_REPO}:${QUAY_TAG} quay.io/${QUAY_ORG}/${QUAY_REPO}:latest
